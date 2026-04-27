66eebd9c096875e0cd5bb6a538aeeae7f428c696
Wojciech Dybowski <dybowski1234@gmail.com>
2026-04-27 19:52:10 +0200

    Data_Models

--- trainme/home/models.py
+++ trainme/home/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Conversation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_created=True)
+    ended_at = models.DateField(null=True, blank=True) 
+    
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    text = models.TextField()
+    from_user = models.BooleanField()
+    count = models.IntegerField()
+    created_at = models.DateField(auto_created=True)
+
+class AudioRecord(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    wav_path = models.FilePathField()
+    created_at = models.DateField(auto_created=True)
+
+
+
+
+
